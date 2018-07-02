42a9233b23a38b8837ccef1334454c73abd787ec
The Mai <Mai.The.09@gmail.com>
2018-07-02 01:12:06 -0500

    remove Link

--- client/src/pages/XPage/XPage.js
+++ client/src/pages/XPage/XPage.js
@@ -1,5 +1,4 @@
 import React, { Component } from 'react';
-import { Link } from 'react-router-dom';
 import { XComponentOne, XComponentTwo } from '../../components/XComponent'
 
 class XPage extends Component {
